0ec50f02712e26152b5ec1d2998548df2354ff83
Kajmix <kacpermizgala1@gmail.com>
2026-03-24 19:39:38 +0100

    beta version
    - file name
    - file size
    - file type
    - creation date

--- filefetch.sh
+++ filefetch.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-echo "Path: $(pwd)"
-echo "Script $0"
-echo "Arg1: $1"
-echo "All args: $@"
-
-echo "File name: $1"
+FILENAME="$1"
+if [ -f "$FILENAME" ]; then
+    FILESIZE=$(stat -c%s "$FILENAME" | numfmt --to=iec --suffix=B)
+    echo "File name: ${FILENAME}"
+    echo "File size: ${FILESIZE}"
+    if [[ "$FILENAME" == *.* ]]; then
+        EXTENCTIONS="${FILENAME##*.}"
+        echo "File type: ${EXTENCTIONS}";
+    fi
+    echo -e "Creation date: $(date -d "$(stat -c %w "$FILENAME")" "+%Y-%m-%d %H:%M:%S")"
+else
+    echo "File not found"
+fi

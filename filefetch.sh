d9371f13a95065320dab5fad8abbc51e5f56ebae
Kajmix <kacpermizgala1@gmail.com>
2026-03-24 20:39:25 +0100

    new functions
    - added author
    - added last modification date
    - added if to check if function file is a photo.
    - added some borders

--- filefetch.sh
+++ filefetch.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 FILENAME="$1"
+
+INFO=(
+    "| File name: ${FILENAME}"
+)
+
+function gap {
+    INFO+=("|-------------------------------------")
+}
+
 if [ -f "$FILENAME" ]; then
     FILESIZE=$(stat -c%s "$FILENAME" | numfmt --to=iec --suffix=B)
-    echo "File name: ${FILENAME}"
-    echo "File size: ${FILESIZE}"
+    gap
+    INFO+=("| File size: ${FILESIZE}")
     if [[ "$FILENAME" == *.* ]]; then
         EXTENCTIONS="${FILENAME##*.}"
-        echo "File type: ${EXTENCTIONS}";
+        INFO+=("| File type: ${EXTENCTIONS}")
     fi
-    echo -e "Creation date: $(date -d "$(stat -c %w "$FILENAME")" "+%Y-%m-%d %H:%M:%S")"
+    INFO+=("| Author: $(stat -c %U "$FILENAME")")
+    INFO+=("| Creation date: $(date -d "$(stat -c %w "$FILENAME")" "+%Y-%m-%d %H:%M:%S")")
+    INFO+=("| Last modification date: $(date -d "$(stat -c %y "$FILENAME")" "+%Y-%m-%d %H:%M:%S")")
+    if [[ "$EXTENCTIONS" == "png" || "$EXTENCTIONS" == "jpg" || "$EXTENCTIONS" == "raw" || "$EXTENCTIONS" == "webp" ]]; then
+        INFO+=("| (Soon)")
+    fi
 else
     echo "File not found"
 fi
+clear
+for line in "${INFO[@]}"; do
+    printf "%s\n" "$line"
+done
+echo
+echo

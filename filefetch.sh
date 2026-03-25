618f98996d20312395f4b121efa7ef874780ac67
Kajmix <kacpermizgala1@gmail.com>
2026-03-25 09:03:58 +0100

    Colors, photo resolution

--- filefetch.sh
+++ filefetch.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 FILENAME="$1"
 
+# Colors:
+GREEN="\033[32m"
+RESET="\033[0m"
+
+#All stored data:
 INFO=(
-    "| File name: ${FILENAME}"
+    "${GREEN}| File name:${RESET} ${FILENAME}"
 )
 
+function get_pixels {
+    resolution=$(file "${FILENAME}" | grep -o "[0-9]\+[[:space:]]*x[[:space:]]*[0-9]\+")
+    INFO+=("${GREEN}| Resolution: ${RESET}${resolution}")
+}
+
 function gap {
-    INFO+=("|-------------------------------------")
+    INFO+=("${GREEN}|-------------------------------------${RESET}")
 }
 
 if [ -f "$FILENAME" ]; then
+    clear
+    #Get file size:
     FILESIZE=$(stat -c%s "$FILENAME" | numfmt --to=iec --suffix=B)
+
     gap
-    INFO+=("| File size: ${FILESIZE}")
+    INFO+=("${GREEN}| Full file path: $(pwd)/${FILENAME}")
+    INFO+=("${GREEN}| File size:${RESET} ${FILESIZE}")
+    
+    #checks if file has extenction
     if [[ "$FILENAME" == *.* ]]; then
         EXTENCTIONS="${FILENAME##*.}"
-        INFO+=("| File type: ${EXTENCTIONS}")
+        INFO+=("${GREEN}| File type:${RESET} ${EXTENCTIONS}")
     fi
-    INFO+=("| Author: $(stat -c %U "$FILENAME")")
-    INFO+=("| Creation date: $(date -d "$(stat -c %w "$FILENAME")" "+%Y-%m-%d %H:%M:%S")")
-    INFO+=("| Last modification date: $(date -d "$(stat -c %y "$FILENAME")" "+%Y-%m-%d %H:%M:%S")")
+    INFO+=("${GREEN}| Author:${RESET} $(stat -c %U "$FILENAME")")
+    INFO+=("${GREEN}| Creation date:${RESET} $(date -d "$(stat -c %w "$FILENAME")" "+%Y-%m-%d %H:%M:%S")")
+    INFO+=("${GREEN}| Last modification date:${RESET} $(date -d "$(stat -c %y "$FILENAME")" "+%Y-%m-%d %H:%M:%S")")
     if [[ "$EXTENCTIONS" == "png" || "$EXTENCTIONS" == "jpg" || "$EXTENCTIONS" == "raw" || "$EXTENCTIONS" == "webp" ]]; then
-        INFO+=("| (Soon)")
+        get_pixels
     fi
 else
-    echo "File not found"
+    INFO=()
+    INFO+=("File not found")
 fi
-clear
-for line in "${INFO[@]}"; do
-    printf "%s\n" "$line"
+#Print all data:
+for line in "${!INFO[@]}"; do
+    echo -e "${INFO[$line]}"
 done
-echo
-echo
